7abfd35b9d205a76393f007dbb6d4d929a0b3875
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2024-11-23 22:34:37 +0530

    Added Index Route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const Listing = require("../Travels/models/listing.js")
+const path = require("path")
 
 const MONGO_URL = "mongodb://localhost:27017/Travel"
 
@@ -15,23 +16,31 @@ async function main(){
     await mongoose.connect(MONGO_URL)
 }
 
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
+
 app.get("/", (req,res)=>{
     res.send("Hi, I am root")
 })
 
-app.get("/testListing", async(req, res)=>{
-    let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "by the lake",
-        price: 1200,
-        location: "Deheradun, UK",
-        country: "India"
-    })
-
-    await sampleListing.save()
-    console.log("Sample was Saved");
-    res.send("Successful")
+app.get("/listings", async(req,res)=>{
+    const allListings = await Listing.find({})
+    res.render("./listings/index.ejs", {allListings})
 })
+
+// app.get("/testListing", async(req, res)=>{
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "by the lake",
+//         price: 1200,
+//         location: "Deheradun, UK",
+//         country: "India"
+//     })
+
+//     await sampleListing.save()
+//     console.log("Sample was Saved");
+//     res.send("Successful")
+// })
 
 app.listen(8080, (req,res)=>{
     console.log("App is listning")
