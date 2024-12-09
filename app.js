282edec90f3b6aa7548e20dbcd086ccbe4141de2
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2024-12-09 10:45:30 +0530

    Shifted db to cloud

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 const ExpressError = require("./utils/ExpressError.js")
 const session = require("express-session")
+const MongoStore = require('connect-mongo')
 const flash = require("connect-flash")
 const passport = require("passport")
 const LocalStrategy = require("passport-local")
@@ -18,8 +19,10 @@ const User = require("./models/user.js")
 const listingRouter = require("./routes/listing.js")
 const reviewRouter = require("./routes/review.js")
 const userRouter = require("./routes/user.js")
+const { error } = require('console')
 
-const MONGO_URL = "mongodb://localhost:27017/Travel"
+// const MONGO_URL = "mongodb://localhost:27017/Travel"
+const dburl = process.env.ATLASDB_URL;
 
 main().then(()=>{
     console.log("Connected To DB");
@@ -28,7 +31,7 @@ main().then(()=>{
 })
 
 async function main(){
-    await mongoose.connect(MONGO_URL)
+    await mongoose.connect(dburl)
 }
 
 app.set("view engine", "ejs")
@@ -38,8 +41,21 @@ app.use(methodOverride("_method"))
 app.engine('ejs', ejsMate)
 app.use(express.static(path.join(__dirname, "/public")))
 
+const store = MongoStore.create({
+    mongoUrl: dburl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+})
+
+store.on("error", () => {
+    console.log("Error in Monogo Session Store", error);
+})
+
 const sessionOptions = {
-    secret: "mysecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
